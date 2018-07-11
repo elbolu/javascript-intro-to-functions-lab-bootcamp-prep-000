f898876a9f7959b7a3f761ccd2649d40b1ee88fd
Leah Hill <leah@verysandwich.com>
2018-07-11 19:29:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
 if (string === string.toLowerCase()) {
   return "I can\'t hear you!"
+<<<<<<< HEAD
 } else if (string === string.toUpperCase()) {
+=======
+} else if (string === string.toUppercase()) {
+>>>>>>> 113d915fd5a6c9c153c1b1f623809c2cdfc8be5b
   return "YES INDEED!"}
   else if (string === "I love you, Grandma.") {
   return "I love you, too."}
